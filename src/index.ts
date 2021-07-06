092a7135d3434b568e0de47d12d8b0c6f03fb54a
pablodanSL <p.salinas@signot.com>
2021-07-06 12:08:27 -0500

    Fix/error al llamar enum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
-import { TiposMensajes } from './tipos.enum'
+
+enum TiposMensajes {
+  SUCCESS = 'Éxito en la operación',
+  ERROR = 'No hubo éxito en la operación',
+  WARNING = 'Probablemente no haya éxito en la operación',
+  INFO = '¿Habrá éxito en la operación? Puede ser'
+}
 
 type MensajeError = {
     httpStatus: StatusCodes,
--- src/tipos.enum.ts
+++
@@ -1,6 +0,0 @@
-export declare enum TiposMensajes {
-  SUCCESS = 'Éxito en la operación',
-  ERROR = 'No hubo éxito en la operación',
-  WARNING = 'Probablemente no haya éxito en la operación',
-  INFO = '¿Habrá éxito en la operación? Puede ser'
-}
